be7b6a36a20b861e31a279f742b474cf87614f3f
Tom Wilkie <tom@grafana.com>
2021-04-13 11:43:04 +0100

    Add test for counters and for instance label.
    Signed-off-by: Tom Wilkie <tom@grafana.com>

---
+++ cases/instance_label.go
@@ -0,0 +1,29 @@
+package cases
+
+import (
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/prometheus/pkg/labels"
+	"github.com/stretchr/testify/require"
+)
+
+// InstanceLabel exports a single metric - the current time - and checks that we receive
+// that metric via remote write, and that it has a instance label that we expect.
+func InstanceLabelTest() Test {
+	return Test{
+		Name: "InstanceLabel",
+		Metrics: metricHandler(prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+			Name: "gauge",
+		}, func() float64 {
+			return 42.0
+		})),
+		Expected: func(t *testing.T, bs []Batch) {
+			// Check we got the metric
+			gauges := countMetricWithValue(t, bs, labels.FromStrings("__name__", "gauge"), 42.0)
+			require.True(t, gauges > 0, `found zero samples for gauge{job="test"}`)
+
+			labelMustMatch(t, bs, "instance", `127.0.0.1:\d+`)
+		},
+	}
+}
