530b23227ef4432622f27a5032d51d09b43f6c56
Julius Volz <julius.volz@gmail.com>
2021-10-11 17:51:13 +0200

    Bring test queries up-to-date with Prometheus 2.26
    Signed-off-by: Julius Volz <julius.volz@gmail.com>

--- promql/testcases/expand.go
+++ promql/testcases/expand.go
@@ -42,11 +42,11 @@ import (
 )
 
 var testVariantArgs = map[string][]string{
-	"range":  {"1s", "15s", "1m", "5m", "15m", "1h"},
-	"offset": {"1m", "5m", "10m"},
-	// TODO: Add "group" aggregator and new duration formats, but it is so new that vendor implementations need time to catch up first.
-	"simpleAggrOp": {"sum", "avg", "max", "min", "count", "stddev", "stdvar"},
-	"topBottomOp":  {"topk", "bottomk"},
+	"range":            {"1s", "15s", "1m", "5m", "15m", "1h", "1h5m15s"},
+	"offset":           {"1m", "5m", "10m", "10m15s"},
+	"simpleAggrOp":     {"sum", "avg", "max", "min", "count", "stddev", "stdvar", "group"},
+	"simpleTimeAggrOp": {"sum", "avg", "max", "min", "count", "stddev", "stdvar", "absent", "last"},
+	"topBottomOp":      {"topk", "bottomk"},
 	"quantile": {
 		"-0.5",
 		"0.1",
@@ -61,7 +61,7 @@ var testVariantArgs = map[string][]string{
 	"arithBinOp":           {"+", "-", "*", "/", "%", "^"},
 	"compBinOp":            {"==", "!=", "<", ">", "<=", ">="},
 	"binOp":                {"+", "-", "*", "/", "%", "^", "==", "!=", "<", ">", "<=", ">="},
-	"simpleMathFunc":       {"abs", "ceil", "floor", "exp", "sqrt", "ln", "log2", "log10", "round"},
+	"simpleMathFunc":       {"abs", "ceil", "floor", "exp", "sqrt", "ln", "log2", "log10", "round", "sgn"},
 	"extrapolatedRateFunc": {"delta", "rate", "increase"},
 	"clampFunc":            {"clamp_min", "clamp_max"},
 	"instantRateFunc":      {"idelta", "irate"},
